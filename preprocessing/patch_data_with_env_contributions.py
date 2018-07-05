40e5488d94e2d27af4dbf1432979c28dd053492a
Mohammad Raji <moa@dragonfli.es>
2018-07-05 00:31:12 -0400

    added more affected species

--- preprocessing/patch_data_with_env_contributions.py
+++ preprocessing/patch_data_with_env_contributions.py
@@ -10,6 +10,19 @@ import sys
 import json
 from docopt import docopt
 
+short_to_long_name = {
+        "con_avg_yearly_rainfall": "cat_avg_yearly_rainfall",
+        "geology": "cat_geology",
+        "soil": "cat_soil_drainage_class",
+        "dist": "cat_type_disturbance",
+        "density": "cat_understory_density_class",
+        "vegetation": "cat_vegetation",
+        "cat_years_since_fire": "cat_years_since_fire",
+        "con_aspect": "con_aspect",
+        "con_solar_rad": "con_direct_solar_radiation",
+        "con_elevation": "con_elevation"
+    }
+
 if __name__ == '__main__':
     arguments = docopt(__doc__)
     with open(arguments['<data_file>']) as fp:
@@ -28,14 +41,16 @@ if __name__ == '__main__':
                 env_stats[i[0]] = [[i[1], i[2]]]
 
     for k, v in data.iteritems():
-        if k in env_stats:
+        if k in short_to_long_name and short_to_long_name[k] in env_stats:
+            _long = short_to_long_name[k]
             data[k]['related-species'] = []
-            for i, species in enumerate(env_stats[k]):
-                latin = env_stats[k][i][0]
-                contrib = env_stats[k][i][1]
+            for i, species in enumerate(env_stats[_long]):
+                latin = env_stats[_long][i][0]
+                contrib = env_stats[_long][i][1]
                 id = irma_mappings[latin]['id']
+                image = "http://via.placeholder.com/150x150"
                 common = irma_mappings[latin]['common']
-                data[k]['related-species'].append([latin, id, common, contrib])
+                data[k]['related-species'].append([latin, id, common, contrib, image])
 
     with open(arguments['<data_file>'], 'w') as fp:
         json.dump(data, fp, indent=4, separators=(',', ': '))
