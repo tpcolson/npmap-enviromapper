b2c3c0dce8d3ffbba8ca985207d5dd86080a65ef
Mohammad Raji <moa@dragonfli.es>
2018-07-30 13:44:28 -0400

    fixed understory density

--- preprocessing/extract_subcategory_understory_density.sh
+++ preprocessing/extract_subcategory_understory_density.sh
@@ -12,17 +12,31 @@ fi
 layer=$2
 variable_name=Dens_Class
 counter=0
-echo -e "Heavy\nMedium\nLight" | 
-while read -r line;
-do
+
+    line="Light"
+    echo $line
+    line=$(echo $line | dos2unix)
+    gdal_rasterize -ot Byte -a_nodata 0 -a OBJECTID -l OGRGeoJSON -ts 3128 1192 -burn 247 -burn 105 -burn 164 -where "$variable_name LIKE '%$line%'" $1 tmp/temp.tif
+    line=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
+    gdal_translate tmp/temp.tif tmp/"$layer"_"$line"_1.tif
+    #gdal_translate -a_srs EPSG:4326 tmp/temp.tif tmp/"$layer"_"$line"_1.tif
+    counter=$(( counter+1 ))
+
+    line="Medium"
     echo $line
     line=$(echo $line | dos2unix)
     gdal_rasterize -ot Byte -a_nodata 0 -a OBJECTID -l OGRGeoJSON -ts 3128 1192 -burn 196 -burn 28 -burn 142 -where "$variable_name LIKE '%$line%'" $1 tmp/temp.tif
     line=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
     gdal_translate tmp/temp.tif tmp/"$layer"_"$line"_1.tif
-    #gdal_translate -a_srs EPSG:4326 tmp/temp.tif tmp/"$layer"_"$line"_1.tif
     counter=$(( counter+1 ))
-done;
+
+    line="Heavy"
+    echo $line
+    line=$(echo $line | dos2unix)
+    gdal_rasterize -ot Byte -a_nodata 0 -a OBJECTID -l OGRGeoJSON -ts 3128 1192 -burn 121 -burn 0 -burn 124 -where "$variable_name LIKE '%$line%'" $1 tmp/temp.tif
+    line=$(echo $line | sed 's/ /_/g' | sed -E -e 's/[;\,-/]/_/g' | sed 's/__/_/g' | sed 's/[)(]//g' | tr '[:upper:]' '[:lower:]')
+    gdal_translate tmp/temp.tif tmp/"$layer"_"$line"_1.tif
+    counter=$(( counter+1 ))
 
 #counter=0
 #echo -e "Heavy\nMedium\nLight" | 
